f2f9380dff9daccbe6a0f6c52322750cf9fbed3d
alvinng4 <alvinng324@gmail.com>
2025-05-21 18:44:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# 100 Days of SwiftUI (2025)
+
+Welcome to my repository for [100 Days of SwiftUI](https://www.hackingwithswift.com/100/swiftui).
+This repository will contain all my projects for this tutorial.
+
+## Completion Status
+
+| Day | Project | Completion Date |
+| --- | ------- | --------------- |
+| 16 - 18 | WeSplit | 21 May 2025  |
+| 19 | Challenge Day | N/A |
+
+<p align="right">
+    <svg width="150" height="150" viewBox="0 0 36 36">
+        <circle
+            cx="18" cy="18" r="16"
+            fill="#eee"
+            stroke="#eee"
+            stroke-width="4"
+        />
+        <path
+            d="M18 2
+                 a 16 16 0 1 1 0 32
+                 a 16 16 0 1 1 0 -32"
+            fill="none"
+            stroke="#4caf50"
+            stroke-width="4"
+            stroke-dasharray="5, 100"
+            stroke-linecap="butt"
+        />
+        <text x="18" y="20.5" text-anchor="middle" font-size="8" fill="#333" font-family="Arial" font-weight="bold">1 / 20</text>
+    </svg>
+</p>
---
+++ WeSplit/WeSplit/WeSplitView.swift
@@ -0,0 +1,99 @@
+import SwiftUI
+
+struct WeSplitView: View
+{
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeopleMinusTwo: Int = 0 // Default and minimum is 2
+    @State private var tipPercentage: Int = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    var numberOfPeople: Int
+    {
+        return numberOfPeopleMinusTwo + 2
+    }
+    
+    var totalAmount: Double
+    {
+        return checkAmount * Double(100 + tipPercentage) / 100.0
+    }
+
+    var totalPerPerson: Double
+    {
+        return totalAmount / Double(numberOfPeople)
+    }
+
+    var body: some View
+    {
+        NavigationStack
+        {
+            Form
+            {
+                Section
+                {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    ).keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeopleMinusTwo)
+                    {
+                        ForEach(2..<100)
+                        {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+
+                Section("Tip percentage")
+                {
+                    Picker("Tip percentage", selection: $tipPercentage)
+                    {
+                        ForEach(0..<101, id: \.self)
+                        {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+
+                Section("Total Amount")
+                {
+                    Text(
+                        totalAmount,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                }
+
+                Section("Amount per person")
+                {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                }
+            }.navigationTitle("WeSplit")
+                .toolbar
+                {
+                    if amountIsFocused
+                    {
+                        Button("Done")
+                        {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview
+{
+    WeSplitView()
+}
